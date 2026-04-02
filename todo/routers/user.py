1dbd0300b82b1ae8bebd26807c44103ce7826e65
sp0ozy <imolchanov210@gmail.com>
2026-04-02 23:47:08 +0300

    error code fix

--- todo/routers/user.py
+++ todo/routers/user.py
@@ -46,7 +46,7 @@ async def change_password(user: user_dependency, db: db_dependency, user_request
         raise HTTPException(status_code=404, detail="User not found")
 
     if not bcrypt_context.verify(user_request.password, user_model.hashed_password):
-        raise HTTPException(status_code=400, detail="Invalid password")
+        raise HTTPException(status_code=401, detail="Invalid password")
 
     user_model.hashed_password = bcrypt_context.hash(user_request.new_password)
     db.add(user_model)
